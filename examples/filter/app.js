d745c7b3fb4f63565547e3144392264ddaa7ee66
Andy Leeper <leepfrog@mochaleaf.com>
2013-03-11 18:14:34 -0700

    Add in a (semi-crummy) example of how to use the pipeline

--- examples/filter/app.js
+++ examples/filter/app.js
@@ -1,2 +1,37 @@
 App = Ember.Application.create();
 
+
+App.BookSelectionFilter = Em.PipePlugin.extend({
+  observes: ['isSelected'],
+  process: function(inputArr){
+    return inputArr.filterProperty('isSelected', true);
+  }
+});
+
+App.Book = Em.Object.extend({
+  title: null,
+  isSelected: false  
+});
+
+App.ApplicationRoute = Em.Route.extend({
+  model: function() {
+    return [
+      App.Book.create({title: "Andy"}),
+      App.Book.create({title: "Mehul"}),
+      App.Book.create({title: "Jeremy"}),
+      App.Book.create({title: "Aaron"}),
+      App.Book.create({title: "Christian"}),
+      App.Book.create({title: "Tiny"}),
+      App.Book.create({title: "Vasilis"}),
+      App.Book.create({title: "Victor"}),
+    ];
+  }  
+})
+
+App.ApplicationController = Em.ArrayController.extend(Em.ArrayPipelineMixin,{
+  plugins: [App.BookSelectionFilter],
+
+  toggleBook: function(book) {
+    book.toggleProperty('isSelected');
+  }
+});
